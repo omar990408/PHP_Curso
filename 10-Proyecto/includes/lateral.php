0e3b98992d9f299f972da38e3e8049941b805f3f
Omar <ofgonzalez1@espe.edu.ec>
2021-11-16 18:46:51 -0500

    Log in made

--- 10-Proyecto/includes/lateral.php
+++ 10-Proyecto/includes/lateral.php
@@ -1,9 +1,29 @@
-
-<?php require_once "helpers.php";?>
+<?php require_once "helpers.php"; ?>
 <asside id="sidebar">
 
+    <?php if (isset($_SESSION['usuario'])): ?>
+        <div id="usuario-logueado" class="bloque">
+
+            <h3> Bienvenido <?= $_SESSION['usuario']['nombre'] . ' ' . $_SESSION['usuario']['apellidos'] ?></h3>
+            <!--            BOTONES-->
+            <a class="boton boton-verde" href="cerrar.php">Crear Entradas</a>
+            <a class="boton" href="cerrar.php">Crear categorias</a>
+            <a class="boton boton-naranja" href="cerrar.php">Mis datos</a>
+            <a class="boton boton-rojo" href="cerrar.php">Cerrar sesión</a>
+        </div>
+    <?php endif; ?>
     <div id="login" class="bloque">
         <h3>Ingreso</h3>
+
+        <?php if (isset($_SESSION['error_login'])): ?>
+            <div class="alerta alerta-error">
+
+                <h3> <?= $_SESSION['error_login']; ?></h3>
+
+            </div>
+        <?php endif; ?>
+
+
         <form action="login.php" method="post">
 
             <label for="email">Email</label>
@@ -21,14 +41,15 @@
         <h3>Registrate</h3>
 
         <?php
+        //var_dump($_SESSION);
         //Mostrar errores
         if (isset($_SESSION['completado'])): ?>
             <div class="alerta alerta-exito">
-                <?=$_SESSION['completado'];?>
+                <?= $_SESSION['completado']; ?>
             </div>
         <?php elseif (isset($_SESSION['errores']['general'])): ?>
-            <div class="alerta alerta-exito">
-                <?=$_SESSION['errores']['general'];?>
+            <div class="alerta alerta-error">
+                <?= $_SESSION['errores']['general']; ?>
             </div>
         <?php endif; ?>
 
@@ -36,19 +57,19 @@
 
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="">
-            <?php echo isset($_SESSION['errores']) ?mostrarError($_SESSION['errores'],'nombre'): ''; ?>
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : ''; ?>
 
             <label for="apellidos">Apellidos</label>
             <input type="text" name="apellidos" id="">
-            <?php echo isset($_SESSION['errores']) ?mostrarError($_SESSION['errores'],'apellidos'): ''; ?>
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'apellidos') : ''; ?>
 
             <label for="email">Email</label>
             <input type="email" name="email" id="">
-            <?php echo isset($_SESSION['errores']) ?mostrarError($_SESSION['errores'],'email'): ''; ?>
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'email') : ''; ?>
 
             <label for="password">Contraseña</label>
             <input type="password" name="password" id="">
-            <?php echo isset($_SESSION['errores']) ?mostrarError($_SESSION['errores'],'password'): ''; ?>
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'password') : ''; ?>
 
             <input type="submit" value="Registrarse">
 
--- 10-Proyecto/registro.php
+++ 10-Proyecto/registro.php
@@ -2,13 +2,16 @@
 
 if (isset($_POST)) {
     require_once "includes/conexion.php";
-    session_start();
+
+    if (!isset($_SESSION)) {
+        session_start();
+    }
     //var_dump($_POST);
     //  RECOGER VALORES DEL FORM
-    $nombre = $_POST['nombre'] ?? false; // isset($_POST['nombre']) ? $_POST['nombre'] : false;
-    $apellidos = $_POST['apellidos'] ?? false;
-    $email = $_POST['email'] ?? false;
-    $password = $_POST['password'] ?? false;
+    $nombre = isset($_POST['nombre'])? mysqli_real_escape_string($db,$_POST['nombre']):false;     // isset($_POST['nombre']) ? $_POST['nombre'] : false;
+    $apellidos = isset($_POST['apellidos'])? mysqli_real_escape_string($db,$_POST['apellidos']):false;
+    $email = isset($_POST['email'])? mysqli_real_escape_string($db,trim($_POST['email'])):false;
+    $password = isset($_POST['password'])? mysqli_real_escape_string($db,$_POST['password']):false;
 
     // ARREGLO DE ERRORES
     $errores = array();
@@ -47,15 +50,17 @@ if (isset($_POST)) {
     if (sizeof($errores) == 0) {
         $guardar_usuario = true;
         //Cifrar Contraseña
-        $password_segura = password_hash($password,PASSWORD_BCRYPT,['cost'=>4]); // ultimo parametro cifra 4 veces
+        $password_segura = password_hash($password, PASSWORD_BCRYPT, ['cost' => 4]); // ultimo parametro cifra 4 veces
         //var_dump(password_verify($password,$password_segura)); // Compara contraseñas
         //Insertar usuario en la BDD en su tabla correspondiente
         $sql = "INSERT INTO usuarios VALUES (null,'$nombre','$apellidos','$email','$password_segura',CURRENT_DATE ())";
-        $guardar = mysqli_query($db,$sql);
-        if($guardar){
+        $guardar = mysqli_query($db, $sql);
+        //var_dump(mysqli_error($db));
+
+        if ($guardar) {
             $_SESSION['completado'] = 'El registro se ha completado con exito';
-        }else{
-            $_SESSION['errores']['general']='Fallo al guardar el usuario!';
+        } else {
+            $_SESSION['errores']['general'] = 'Fallo al guardar el usuario!';
         }
     } else {
         $_SESSION['errores'] = $errores;
